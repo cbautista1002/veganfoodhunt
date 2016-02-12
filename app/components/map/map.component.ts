0b837072d99b1c1e5d606ce0a3d532cf7f9b46c5
Carlos Bautista <cbautista@ise.com>
2016-02-12 13:49:03 -0500

    Add angular2-google-maps

--- app/components/map/map.component.ts
+++ app/components/map/map.component.ts
@@ -1,22 +1,38 @@
 import {Component} from 'angular2/core';
+// Load all directives from angular2-google-maps
+import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
+
 
 @Component({
     selector: 'map-view',
+    directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
     templateUrl: 'app/components/map/map.component.html',
     styleUrls: ['app/bootstrap.css', 'app/components/map/map.component.css']
 })
 export class MapComponent {
 
+  lat: number;
+  lng: number;
+  zoom: number;
+
   constructor(){
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(40.781346, -73.966614);
-    var mapOptions = {
-      zoom: 12,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
-    }
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    this.zoom = 12;
+    this.getLocation();
   }
 
+  getLocation(){
+    let options = {timeout: 10000, enableHighAccuracy: true};
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      },
+      options
+    );
+  }
 }
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 ///<reference path="../node_modules/angular2/platform/browser.d.ts"/>
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
